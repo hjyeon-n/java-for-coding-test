911f8cd353af2d3453ca70213c637cd6d62f6f60
kong0527 <o____ri@naver.com>
2021-05-06 00:07:30 +0900

    바닥 공사 solution

---
+++ Dynamic Programming/kong0527/개미 전사.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+        int[] dp = new int[num];
+        String st[] = br.readLine().split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st[i]);
+        }
+
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
+        }
+
+        System.out.println(dp[num-1]);
+    }
+}
