43d953514d5272fe00bd63c953e7c44074fcb10a
hjyeon-n <hyunjy25@gmail.com>
2021-05-12 12:48:38 +0900

    안테나 solution

---
+++ Sort/hjyeon-n/기출 문제/안테나.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		//		소요 시간 : 15분
+		//		체감 난이도 : 실버 4
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[(N - 1) / 2]);
+	}
+}
